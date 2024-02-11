6f0a25029d1a4985ea7bf1c957081780aabba75d
Pranav Prakash <pranavpprakash7@gmail.com>
2024-02-11 11:19:16 +0530

    button_click_feedback

--- ui/button_type1.py
+++ ui/button_type1.py
@@ -8,26 +8,38 @@ class ButtonType1:
         self.rect = rect
         self.callback = callback
         self.button_color = (50, 50, 50)
-        self.button_color_border = (100, 100, 100)
-        self.hover_color_border = (255, 255, 255)  # New color for hover
+        self.original_color_border = (100, 100, 100)
+        self.hover_color_border = (255, 255, 255)
+        self.click_color = (75, 75, 75)  # New color for click
         self.button_radius = 5
-        self.hovered = False  # Track hover state
+        self.hovered = False
+        self.clicked = False  # Track click state
 
     def handle_event(self, event):
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_pos = pygame.mouse.get_pos()
             if self.rect.collidepoint(mouse_pos) and self.callback:
+                self.clicked = True
                 self.callback()
+        elif event.type == pygame.MOUSEBUTTONUP:
+            self.clicked = False
         elif event.type == pygame.MOUSEMOTION:
-            # Check if the mouse is over the button
             self.hovered = self.rect.collidepoint(event.pos)
 
     def draw(self):
-        # Change border color based on hover state
-        border_color = self.hover_color_border if self.hovered else self.button_color_border
+        # Change border color and button color based on click and hover states
+        if self.clicked:
+            border_color = self.original_color_border
+            button_color = self.click_color
+        elif self.hovered:
+            border_color = self.hover_color_border
+            button_color = self.button_color
+        else:
+            border_color = self.original_color_border
+            button_color = self.button_color
 
         # Draw the button
-        pygame.draw.rect(self.screen, self.button_color, self.rect, border_radius=self.button_radius)
+        pygame.draw.rect(self.screen, button_color, self.rect, border_radius=self.button_radius)
         pygame.draw.rect(self.screen, border_color, self.rect, border_radius=self.button_radius, width=1)
         self.draw_text()
 
